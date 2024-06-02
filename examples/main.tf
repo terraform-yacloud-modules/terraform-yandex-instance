485cdb8aa579173cf5b5f40931bf9cf246040a06
Anton Patsev <patsev.anton@gmail.com>
2024-06-02 18:30:52 +0600

    Add example

---
+++ examples/main.tf
@@ -0,0 +1,47 @@
+module "yandex_compute_instance" {
+  source = "../"
+
+  folder_id = "xxx"
+
+  name        = "my-instance"
+  description = "Test instance"
+  labels = {
+    environment = "dev"
+    project     = "example"
+  }
+
+  zone                      = "ru-central1-a"
+  subnet_id                 = "xxx"
+  enable_nat                = true
+  create_pip                = true
+  network_acceleration_type = "standard"
+  serial_port_enable        = false
+
+  platform_id   = "standard-v3"
+  cores         = 2
+  memory        = 4
+  core_fraction = 100
+
+  preemptible = false
+
+  image_id = "fd8dpupkt886ut5m0j2o" # ubuntu-2204-lts
+
+  hostname                  = "my-instance"
+  allow_stopping_for_update = true
+  generate_ssh_key          = false
+  ssh_user                  = "ubuntu"
+  ssh_pubkey                = "~/.ssh/id_magnit.pub"
+
+  boot_disk = {
+    auto_delete = true
+    device_name = "boot-disk"
+    mode        = "READ_WRITE"
+  }
+
+  boot_disk_initialize_params = {
+    size       = 20
+    block_size = 4096
+    type       = "network-ssd"
+  }
+
+}
--- pip.tf
+++ pip.tf
@@ -1,7 +1,8 @@
 resource "yandex_vpc_address" "main" {
   count = var.create_pip ? 1 : 0
 
-  name = var.name
+  name      = var.name
+  folder_id = var.folder_id
 
   external_ipv4_address {
     zone_id = var.zone
