9e9b8caa44eefc36fbf49b6729c9191093852a04
Anton Patsev <patsev.anton@gmail.com>
2024-07-22 11:23:56 +0600

    create minimal

--- examples/full/main.tf
+++ examples/full/main.tf
@@ -46,9 +46,9 @@ module "yandex_compute_instance" {
 }
 
 output "instance_private_ip" {
-  value       = module.yandex_compute_instance.instance_private_ip
+  value = module.yandex_compute_instance.instance_private_ip
 }
 
 output "instance_public_ip" {
-  value       = module.yandex_compute_instance.instance_public_ip
+  value = module.yandex_compute_instance.instance_public_ip
 }
---
+++ examples/minimal/main.tf
@@ -0,0 +1,26 @@
+module "yandex_compute_instance" {
+  source = "../"
+
+  folder_id = "xxxx"
+
+  name = "my-instance"
+
+  zone       = "ru-central1-a"
+  subnet_id  = "xxxx"
+  enable_nat = true
+  create_pip = true
+
+  hostname         = "my-instance"
+  generate_ssh_key = false
+  ssh_user         = "ubuntu"
+  ssh_pubkey       = "~/.ssh/id_rsa.pub"
+
+}
+
+output "instance_private_ip" {
+  value = module.yandex_compute_instance.instance_private_ip
+}
+
+output "instance_public_ip" {
+  value = module.yandex_compute_instance.instance_public_ip
+}
