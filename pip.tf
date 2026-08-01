62a13c08847181150f73bbd2313d5d8971aabb30
Alexander Sharov <kvendingoldo@yandex.ru>
2026-08-01 23:42:47 +0400

    feat: add dynamic network_interface

--- pip.tf
+++ pip.tf
@@ -1,11 +1,29 @@
 resource "yandex_vpc_address" "main" {
-  count = var.create_pip ? 1 : 0
+  for_each = {
+    for idx, ni in var.network_interfaces :
+    idx => ni
+    if lookup(ni, "nat", false) && lookup(ni, "nat_ip_address", null) == null
+  }
 
-  name      = var.name
-  folder_id = var.folder_id
-  labels    = var.labels
+  name                = format("%s-%s", var.name, each.key)
+  description         = try(each.value.pip.description, null)
+  folder_id           = var.folder_id
+  labels              = var.labels
+  deletion_protection = try(each.value.pip.deletion_protection, null)
 
   external_ipv4_address {
-    zone_id = var.zone
+    zone_id                  = try(each.value.pip.ddos_protection_provider, var.zone, null)
+    ddos_protection_provider = try(each.value.pip.ddos_protection_provider, null)
+    outgoing_smtp_capability = try(each.value.pip.outgoing_smtp_capability, null)
+  }
+
+  dynamic "dns_record" {
+    for_each = try(each.value.pip.dns_record, null) != null ? [each.value.pip.dns_record] : []
+    content {
+      fqdn        = dns_record.value.fqdn
+      dns_zone_id = dns_record.value.dns_zone_id
+      ttl         = lookup(dns_record.value, "ttl", null)
+      ptr         = lookup(dns_record.value, "ptr", false)
+    }
   }
 }
